e84ca144ad2d53a8ba54c5412c31980a9efeefc3
Ajimi Mahdi <ajimimahdi1@gmail.com>
2026-03-26 14:42:20 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,11 @@
+const
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+  res.send('Server del mio blog');
+});
+
+app.listen(port, () => {
+  console.log(`il mio blog è in esecuzione su http://localhost:${port}`);
+});
